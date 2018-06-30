d704503f17d7afe4bca706c261a6ddb2b3384ef9
wasu <wevorence@gmail.com>
2018-06-30 10:15:03 +0000

    correspondence to result display

--- main.rb
+++ main.rb
@@ -26,13 +26,10 @@ class MatchPair
     "*** #{first.name} vs #{after.name} #{inning}***\n#{@first.to_string}#{@after.to_string}"
   end
   def team_in_pair(name)
-    @first.name == name || @after.name == name
+    (@first.name == name) || (@after.name == name)
   end
   def scores_equal(first_score,after_score)
-    @first.score == first_score || @after.score == after_score
-  end
-  def scores_equal(mp)
-    @first.score == mp.first.score || @after.score == mp.after.score
+    (@first.score == first_score) && (@after.score == after_score)
   end
   # 順番関係ないゆるい比較。
   def slack_scores_equal(scoreA,scoreB)
@@ -59,30 +56,33 @@ MAIN_URL = "https://baseball.yahoo.co.jp"
 mps=[]
 loop do
   doc = Nokogiri::HTML(open("#{MAIN_URL}/npb/"))
-  Slack.chat_postMessage(channel: BOT_CHANNEL,
+  p Slack.chat_postMessage(channel: BOT_CHANNEL,
                            text: "*****  #{Time.new.strftime("%Y-%m-%d %H:%M:%S")}  *****")
-  doc.css("table.score a").each{|n|
-    match_page = Nokogiri::HTML(open("#{MAIN_URL}#{n.attr("href")}"))
-    titles = match_page.title.split(' ') # temaname is 1 3
-    scores = match_page.css("td.sum").map{|s|
-      s.text
-    }
+  doc.css("table td.yjMSt a").each{|n|
+    href = n.attr("href") + "/top"
+    match_page = Nokogiri::HTML(open("#{MAIN_URL}#{href}"))
+    first_team_name = match_page.css("tr#tb1 th a").text
+    after_team_name = match_page.css("tr#tb2 th a").text
+
+    scores = match_page.css("td.sum").map{|s| s.text }
     if !scores.first.nil?
-      cur_mp = mps.find{|m| m.team_in_pair(titles[1])}
+      cur_mp = mps.find{|m| m.team_in_pair(first_team_name)}
       inning = n.text()
       if !cur_mp
-        cur_mp = MatchPair.new(Team.new(titles[1],scores[0]),
-                               Team.new(titles[3],scores[1]),
+        cur_mp = MatchPair.new(Team.new(first_team_name,scores[0]),
+                               Team.new(after_team_name,scores[1]),
                                inning)
-        mps = mps.push
+        mps = mps.push cur_mp
         p cur_mp.send_to_slack_to_string(BOT_CHANNEL)
-      elsif !cur_mp.slack_scores_equal(scores[0],scores[1])
+      elsif !cur_mp.scores_equal(scores[0],scores[1])
         cur_mp.first.score = scores[0]
         cur_mp.after.score = scores[1]
+        cur_mp.inning = inning
         p cur_mp.send_to_slack_to_string(BOT_CHANNEL)
       end
       sleep(1)
     end
+
   }
   sleep(10 * 30)
 end
