3cce164891c96d397c0116ce5ac3866d256b1f86
wasu <wevorence@gmail.com>
2018-06-30 01:59:22 +0000

    added function reading config and send to slack.

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem "nokogiri"
-
+gem "slack-api"
+gem "parseconfig"
 # gem "rails"
--- main.rb
+++ main.rb
@@ -1,7 +1,20 @@
+# coding: utf-8
 require "nokogiri"
 require "open-uri"
+require "slack"
+require "parseconfig"
 
-class MatchResult
+MYCONF = ParseConfig.new('config')
+BOT_CHANNEL=MYCONF['bot_channel']
+
+SLACK_TOKEN = MYCONF['slack_token']
+
+# 余計な文字が入るので
+Slack.configure do |config|
+  config.token = SLACK_TOKEN.gsub(/\n/,"")
+end
+# 対戦ペアクラス
+class MatchPair
   attr_accessor :first,:after
   def initialize(first_name,after_name,first_score,after_score)
     @first = Team.new(first_name,first_score)
@@ -9,6 +22,22 @@ class MatchResult
   end
   def to_string()
     "*** #{first.name} vs #{after.name} ***\n#{@first.to_string}#{@after.to_string}"
+  end
+  def team_in_pair(name)
+    @first.name == name || @after.name == name
+  end
+  def scores_equal(first_score,after_score)
+    @first.score == first_score || @after.score == after_score
+  end
+  def scores_equal(mp)
+    @first.score == mp.first.score || @after.score == mp.after.score
+  end
+  # 順番関係ないゆるい比較。
+  def slack_scores_equal(scoreA,scoreB)
+    score_equal(scoreA,scoreB) || score_equal(scoreB,scoreA)
+  end
+  def send_to_slack_to_string(channel)
+    Slack.chat_postMessage(channel: channel,text: to_string)
   end
 end
 
@@ -25,18 +54,26 @@ end
 
 MAIN_URL = "https://baseball.yahoo.co.jp"
 
-teams=[]
-doc = Nokogiri::HTML(open("#{MAIN_URL}/npb/"))
-doc.css("table.score a").each{|n|
-  match_page = Nokogiri::HTML(open("#{MAIN_URL}#{n.attr("href")}"))
-  titles = match_page.title.split(' ') # temaname is 1 3
-  scores = match_page.css("td.sum").map{|s|
-    s.text
+mps=[]
+loop do
+  doc = Nokogiri::HTML(open("#{MAIN_URL}/npb/"))
+  doc.css("table.score a").each{|n|
+    match_page = Nokogiri::HTML(open("#{MAIN_URL}#{n.attr("href")}"))
+    titles = match_page.title.split(' ') # temaname is 1 3
+    scores = match_page.css("td.sum").map{|s|
+      s.text
+    }
+    if !scores.first.nil?
+      cur_mp = mps.find{|m| m.team_in_pair(titles[1])}
+      if !cur_mp
+        mps = mps.push MatchPair.new(titles[1],titles[3],scores[0],scores[1])
+      elsif !cur_mp.slack_scores_equal(scores[0],scores[1])
+        cur_mp.first.score = scores[0]
+        cur_mp.after.score = scores[1]
+        cur_mp.send_to_slack_to_string(BOT_CHANNEL)
+      end
+      sleep(1)
+    end
   }
-  teams = teams.push MatchResult.new(titles[1],titles[3],scores[0],scores[1])
-  sleep(1)
-}
-#p teams
-teams.each{|team|
-  puts team.to_string
-}
+  sleep(10 * 60)
+end
